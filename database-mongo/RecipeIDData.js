2933cd34405a640441c3f396ce73279a594d7136
gkolb <gkolb100@gmail.com>
2018-02-21 16:55:19 -0800

    Set up server to serve fake data

---
+++ database-mongo/RecipeIDData.js
@@ -0,0 +1,9 @@
+var sampleRecipe = {
+  "id": 65597,
+  "title": "Cinnamon Streusel Muffins",
+  "image": "https://spoonacular.com/recipeImages/65597-312x231.jpg",
+  "likes": 0,
+  "ingredients": ['ingredient1', 'ingredient2', 'ingredient3', 'ingredient4', 'ingredient5','ingredient6', 'ingredient7', 'ingredient8']
+}
+
+module.exports.sampleRecipe = sampleRecipe;
--- server/index.js
+++ server/index.js
@@ -1,2 +1,18 @@
-//comment - edited
-//new comment
+const express = require('express');
+const app = express();
+const bodyParser = require('body-parser');
+const recipe = require('../database-mongo/RecipeIDData.js');
+const recipeList = require('../database-mongo/RecipeListData.js');
+
+
+app.use(express.static(__dirname + '/../client/dist'));
+
+app.post('/db/save', (req, res) => res.send('save to db'))
+app.get('/db/fetch', (req, res) => res.send('favorites list'))
+app.get('/recipeByIngredient', (req, res) => res.send(recipeList.recipeObj.fakeRecipes))
+app.get('/recipeById', (req, res) => res.send(recipe.sampleRecipe))
+app.post('/sendText', (req, res) => res.send('messag sent'))
+
+
+
+app.listen(3000, () => console.log('Example app listening on port 3000!'))
