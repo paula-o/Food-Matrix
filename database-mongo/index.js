ff909e719061ca791f3c6a23847ed37c23efcb65
gkolb <gkolb100@gmail.com>
2018-02-22 16:37:37 -0800

    Set up database with promises

--- database-mongo/index.js
+++ database-mongo/index.js
@@ -1,8 +1,10 @@
 const mongoose = require('mongoose');
+const Promise = require("bluebird");
 
-mongoose.connect();
+mongoose.connect('mongodb://localhost/foodmatrix');
 
 const db = mongoose.connection;
+
 
 db.on('error', console.error.bind(console, 'MongoDB connection error:'));
 
@@ -32,21 +34,39 @@ let save = (documentObj) => {
                                     imageUrl: documentObj.recipe.image,
                                     likes: documentObj.recipe.likes
   });
-  document.save(function(err, repo) {
+  document.save(function(err, favorite) {
     if (err) return console.error(err);
   });
 };
+// save({
+//   username:'greg',
+//   recipe: {
+//     id:1242414,
+//     title:'food',
+//     image:'awsomepic.jpg',
+//     likes:61928469
+//   }
+// });
 
 //Returning list of a user's favorites, sorted descending by popularity('likes')
-let retrieve = (username, callback) => {
-  var query = Repo.find({ 'username' : username });
-  query.select({});
-  query.limit(10);
-  query.sort({ likes: -1 });
-  query.exec(function(err, favorites) {
-    callback(favorites);
-  });
+let retrieve = (username) => {
+  return new Promise(function(resolve, reject) {
+    var query = UserFavorite.find({ 'username' : username });
+    query.select({});
+    query.limit(10);
+    query.sort({ likes: -1 });
+    query.exec(function(err, favorites) {
+      if (err) {
+        reject(err);
+      } else {
+        resolve(favorites);
+      }
+    });
+  })
 };
+
+// retrieve('greg')
+// .then(console.log)
 
 module.exports = {
   save : save,
